9de24a0b82976abb507dee0527abea39e1f464b6
Saghir786 <saghir786@gmail.com>
2025-11-08 22:15:51 +0000

    PWA fix

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,32 @@
-const CACHE_NAME='gym-routine-mp-v1';
-const ASSETS=['./','./index.html','./day1.html','./day2.html','./day3.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))));self.clients.claim();});
-self.addEventListener('fetch',e=>{const url=new URL(e.request.url);if(url.origin===location.origin){e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request)));}else{e.respondWith(fetch(e.request));}});
+const CACHE_NAME = 'gym-routine-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './day1.html',
+  './day2.html',
+  './day3.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => k !== CACHE_NAME ? caches.delete(k) : null)))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+  if (url.origin === location.origin) {
+    event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+  } else {
+    event.respondWith(fetch(event.request));
+  }
+});
